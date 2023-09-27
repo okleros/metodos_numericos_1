ed68307bd02e2addecd96dd09431bf8cdf1b0816
Gutemberg Andrade <105687031+okleros@users.noreply.github.com>
2023-09-27 10:57:09 -0300

    dfsg

--- bisection.cpp
+++ bisection.cpp
@@ -10,7 +10,7 @@ int main(int argc, char const *argv[])
 {
 	try
 	{
-		printf("%.8Lf\n", bisection(&f, 3, 4, 1e-4));
+		printf("%.8Lf\n", bisection(&f, 3, 4, 1e-5));
 	}
 	catch (const std::runtime_error &e)
 	{
